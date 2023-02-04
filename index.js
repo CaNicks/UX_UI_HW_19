3da414e3dc02e7910006962666b4a082e00000b5
CaNicks <ashytaria@gmail.com>
2023-02-03 17:45:26 -0700

    jQuery

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Your Index.js file is loaded")
+
+$(".item2").ready(function(){
+    $(".item").click(function(){
+      $(".item2").show("slide", { direction: "right" }, 1000);
+    });
+  });
+
+  $(".item3").ready(function(){
+    $(".Picture4").click(function(){
+      $("#panel").show("slide", { direction: "right" }, 1000);
+    });
+  });
