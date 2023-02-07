4f019ce1034707aa1897f10c3ccb7d17eeeee305
CaNicks <ashytaria@gmail.com>
2023-02-06 18:05:34 -0700

    jQuery Fix

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 console.log("Your Index.js file is loaded")
+  $(".item2").hide();
+  $(".item3").hide();
 
-$(".item2").ready(function(){
-    $(".item").click(function(){
-      $(".item2").show("slide", { direction: "right" }, 1000);
-    });
-  });
 
-  $(".item3").ready(function(){
-    $(".Picture4").click(function(){
-      $("#item3").show("slide", { direction: "right" }, 1000);
+
+    $(".item").on( "click", function(){
+      $(".item3").show("slide", { direction: "right" }, 1000);
     });
-  });
+  
+
+
+    $(".Picture4").on("click", function(){
+      $(".item2").show("slide", { direction: "left" }, 1000);
+      
+    });
+  
 
   console.log(".item2")
+
+  
